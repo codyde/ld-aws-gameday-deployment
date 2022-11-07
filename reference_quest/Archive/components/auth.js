b144dfc99514e9e3c069a4474f5ecb95df801ea1
Cody De Arkland <codydearkland@gmail.com>
2022-11-07 13:40:24 -0800

    removing archive folder

--- reference_quest/Archive/components/auth.js
+++
@@ -1,8 +0,0 @@
-export const isAuthenticated = () => localStorage.getItem('user_key') !== null;
-export const getToken = () => localStorage.getItem('user_key');
-export const login = user => {
-    localStorage.setItem('user_key', JSON.stringify(user));
-};
-export const logout = () => {
-    localStorage.clear();
-};
--- reference_quest/Archive/config.py
+++
@@ -1,6 +0,0 @@
-class ApplicationConfig:
-    JSONIFY_PRETTYPRINT_REGULAR = True
-    DEBUG = True
-    SESSION_PERMANENT = False
-    SESSION_TYPE = 'filesystem'
-    SECRET_KEY = 'asdfjalkjfewiajf'
